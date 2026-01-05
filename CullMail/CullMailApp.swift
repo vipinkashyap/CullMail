6b846c11034a1e85254c27584c75181a8fa17512
Vipin Kashyap <kaizen.labsindia@gmail.com>
2026-01-04 20:46:30 -0800

    Initial Commit

---
+++ CullMail/CullMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  CullMailApp.swift
+//  CullMail
+//
+//  Created by Vipin Kumar Kashyap on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CullMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
